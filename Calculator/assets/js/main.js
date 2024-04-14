21976c8024f358534c1b715b0d5e81645864f219
Maruf Mobin <mobinh264@gmail.com>
2024-04-14 23:52:54 +0600

    Calculator Create Done

---
+++ Calculator/assets/js/main.js
@@ -0,0 +1,38 @@
+let inputButtonOne;
+let inputButtonTwo;
+let selectedButton;
+
+function inputFieldOne() {
+    let x = document.getElementById("inputFieldOne");
+    inputButtonOne = x.value;
+  }
+
+  function inputFieldTwo() {
+    let x = document.getElementById("inputFieldTwo");
+    inputButtonTwo = x.value;
+  }
+
+function optionValue(){
+    let x = document.getElementById("optionValue");
+    selectedButton = x.value;
+}
+
+function submitButton(){
+    if( selectedButton === 'add'){
+       let res =  Number(inputButtonOne) + Number(inputButtonTwo)
+       document.getElementById("resultBox").innerHTML = res;
+    }
+   else if( selectedButton === 'sub'){
+        let res =  Number(inputButtonOne) - Number(inputButtonTwo)
+        document.getElementById("resultBox").innerHTML = res;
+    }
+    else if( selectedButton === 'mul'){
+        let res =  Number(inputButtonOne) * Number(inputButtonTwo)
+       document.getElementById("resultBox").innerHTML = res;
+    }
+    else if( selectedButton === 'division'){
+        let res =  Number(inputButtonOne) / Number(inputButtonTwo)
+       document.getElementById("resultBox").innerHTML = res;
+    }
+}
+
